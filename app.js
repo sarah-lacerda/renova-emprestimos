3f91f21c63e7fc04cf0d32631b780a09884cc6a0
Sarah Lacerda <sarah.lacerda@mail.com>
2021-05-19 13:50:30 -0300

    Remove unnecessary code and add TODO

--- app.js
+++ app.js
@@ -22,27 +22,14 @@ async function login() {
     await dropDown({ name: 'idCategoriaHtml' }).select('42');
     await press('Tab')
     await press('Tab')
-   // await focus(textBox({ name:'cdMatriculaHtml:'}));
     await write(credentials.matricula);
     await press('Tab')
     await write(credentials.senha);
     await press('Enter');
 }
 
-async function marcacaoDia({ dia, entrada, saida }) {
-    await click($('[data-marcacao="' + dia + '"] [data-horaprimeramarcacao] input'));
-    await press('Enter');
-    await write(entrada);
-    await click('Home Office');
-    await click('Confirmar');
-    await sleep(2000);
-
-    await click($('[data-marcacao="' + dia + '"] [data-horasegundamarcacao] input'));
-    await press('Enter');
-    await write(saida);
-    await click('Home Office');
-    await click('Confirmar');
-    await sleep(2000);
+async function renova() {
+    // TODO: Implement me
 }
 
 (async () => {
@@ -52,19 +39,8 @@ async function marcacaoDia({ dia, entrada, saida }) {
         });
         await goto("https://biblioteca.pucrs.br/renovacao");
         await login();
+        // await renova();
         await sleep(2000);
-
-
-/*        await click($("#op690"));
-        await click("Apontamento");
-        await focus($('#contentPaginasInternas_txtAnoMes'));
-        await clear($('#contentPaginasInternas_txtAnoMes'));
-        await write(mesAno);
-        await press('Enter');
-
-        for await (d of apontamento) {
-            await marcacaoDia(d)
-        }*/
     } catch (error) {
         console.error(error);
     } finally {
